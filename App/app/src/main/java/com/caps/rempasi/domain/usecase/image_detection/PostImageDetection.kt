dddc11bdea368b2af169176f90e704dde9c94485
AkhmadhetaHPras <akhmadhetahafid@gmail.com>
2023-06-08 11:26:55 +0700

    remove image ratio

--- App/app/src/main/java/com/caps/rempasi/domain/usecase/image_detection/PostImageDetection.kt
+++ App/app/src/main/java/com/caps/rempasi/domain/usecase/image_detection/PostImageDetection.kt
@@ -1,8 +1,9 @@
 package com.caps.rempasi.domain.usecase.image_detection
 
-import com.caps.rempasi.presentation.ui.screen.recomendation.RecommendationResult
+import com.caps.rempasi.data.remote.response.PredictionsItem
+import com.caps.rempasi.presentation.ui.screen.recomendation.DetectionResult
 import kotlinx.coroutines.flow.Flow
 
 interface PostImageDetection {
-    fun imageDetection(): Flow<RecommendationResult>
+    fun imageDetection(): Flow<List<PredictionsItem>>
 }
--- App/app/src/main/java/com/caps/rempasi/utils/ImageHelper.kt
+++ App/app/src/main/java/com/caps/rempasi/utils/ImageHelper.kt
@@ -5,6 +5,8 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Environment
+import android.provider.MediaStore
+import androidx.core.content.FileProvider
 import java.io.*
 import java.text.SimpleDateFormat
 import java.util.*
@@ -39,36 +41,36 @@ object ImageHelper {
         return myFile
     }
 
-    fun resizeImage(context: Context, uri: Uri): File? {
-        try {
-            // Load the original image from the Uri
-            val inputStream = context.contentResolver.openInputStream(uri)
-            val originalBitmap = BitmapFactory.decodeStream(inputStream)
-
-            // Calculate the target dimensions for the resized image
-            val targetWidth = originalBitmap.height * 3 / 4
-            val targetHeight = originalBitmap.height
-
-            // Create a new Bitmap with the target dimensions
-            val resizedBitmap = Bitmap.createScaledBitmap(originalBitmap, targetWidth, targetHeight, false)
-
-            // Create a new File to save the resized image
-            val resizedFile = File(context.cacheDir, "resized_image.jpg")
-            val outputStream = FileOutputStream(resizedFile)
-
-            // Compress the resized Bitmap into the FileOutputStream
-            resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
-
-            // Close the streams
-            inputStream?.close()
-            outputStream.close()
-
-            return resizedFile
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-        return null
-    }
+//    fun resizeImage(context: Context, uri: Uri): File? {
+//        try {
+//            // Load the original image from the Uri
+//            val inputStream = context.contentResolver.openInputStream(uri)
+//            val originalBitmap = BitmapFactory.decodeStream(inputStream)
+//
+//            // Calculate the target dimensions for the resized image
+//            val targetWidth = originalBitmap.height * 3 / 4
+//            val targetHeight = originalBitmap.height
+//
+//            // Create a new Bitmap with the target dimensions
+//            val resizedBitmap = Bitmap.createScaledBitmap(originalBitmap, targetWidth, targetHeight, false)
+//
+//            // Create a new File to save the resized image
+//            val resizedFile = File(context.cacheDir, "resized_image.jpg")
+//            val outputStream = FileOutputStream(resizedFile)
+//
+//            // Compress the resized Bitmap into the FileOutputStream
+//            resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+//
+//            // Close the streams
+//            inputStream?.close()
+//            outputStream.close()
+//
+//            return resizedFile
+//        } catch (e: IOException) {
+//            e.printStackTrace()
+//        }
+//        return null
+//    }
 
     fun reduceFileImage(file: File): File {
         val bitmap = BitmapFactory.decodeFile(file.path)
